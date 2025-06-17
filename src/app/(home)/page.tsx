80eb1859993ca710c3385ca8fede4de229a3a5d7
waqas831 <waqasishaq800@gmail.com>
2025-06-17 10:22:35 +0500

    dashboard set

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import GlobeResidencyPage from './_components/overview-cards/GlobeResidencyPage'
+import { ProjectCards } from './_components/ProjectCards'
 
 const page = () => {
   return (
     <>
-      <div>
-        <h2 className='text-2xl font-semibold'>DashBoard</h2>
-        <GlobeResidencyPage />
+      <div className="p-4">
+        <h2 className='text-2xl font-semibold mb-6'>Dashboard</h2>
+        <ProjectCards />
       </div>
     </>
   )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,9 @@
 "use client"
-import { OverviewCardsGroup } from './(home)/_components/overview-cards';
 import Home from './(home)/page';
-import CounterDisplay from './components/CounterDisplay';
 
 export default function HomePage() {
     return (
         <main>
-            {/*
-             <CounterDisplay />
-             <OverviewCardsGroup />
-            */}
             <Home />
         </main>
     );
