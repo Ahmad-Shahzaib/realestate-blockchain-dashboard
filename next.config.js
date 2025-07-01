d8443335f919faf1023ffbcca9e8d541533ed462
waqas-softsuite <waqas@softsuitetech.com>
2025-07-01 17:35:51 +0500

    upgrade nextjs version

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Disable TypeScript type checking
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Disable ESLint
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
