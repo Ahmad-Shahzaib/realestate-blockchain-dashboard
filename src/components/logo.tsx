71a752d43a72708abff684e0bdb01b546a8e4438
shakeelsoftsuite <shakeel@softsuitetech.com>
2025-08-19 17:34:46 +0500

    `Update Sidebar and Logo components: remove unnecessary class, adjust logo image and styles`

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,28 +1,10 @@
-import darkLogo from "@/assets/logos/dark.svg";
-import logo from "../assets/logos/fci.jpg";
 import Image from "next/image";
+import logo from "../assets/logos/fractprop.png";
 
 export function Logo() {
   return (
-    <div className="relative h-9 max-w-[8.847rem]">
-      <Image
-        src={logo}
-        fill
-        className="dark:hidden rounded-md "
-        alt="Dao logo"
-        role="presentation"
-        quality={100}
-
-      />
-
-      <Image
-        src={logo}
-        fill
-        className="hidden dark:block"
-        alt="Dao logo"
-        role="presentation"
-        quality={100}
-      />
+    <div className="relative h-9 pt-2 max-w-[8.847rem]">
+      <Image src={logo} alt="logo" className="object-contain" />
     </div>
   );
 }
