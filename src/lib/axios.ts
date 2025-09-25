35ca117f51994fde2bed6af1281d870efcf81b9a
shakeelayazjoya <shakeelayaz954@gmail.com>
2025-09-25 17:33:36 +0500

    `Update axios base URL to https://api.fractprop.com`

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 import { getCookie, deleteCookie } from "cookies-next";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://api.fractprop.com";
 const instanceCache: { [key: string]: AxiosInstance } = {};
 
 export function getAxiosInstance(serviceName: string, version = "1.0.0") {
