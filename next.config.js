3aa8cdb01759b75c6aa653dd669a9d03d455c671
waqas-softsuite <waqas@softsuitetech.com>
2025-10-20 16:04:11 +0500

    fixing build issues

--- next.config.js
+++ next.config.js
@@ -26,12 +26,22 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev',
       },
-        {
-          protocol: 'https',
-          hostname: 'fractprop.s3.eu-north-1.amazonaws.com',
-        },
+      {
+        protocol: 'https',
+        hostname: 'fractprop.s3.eu-north-1.amazonaws.com',
+      },
     ],
   },
+  // Increase the timeout for static page generation (in seconds)
+  staticPageGenerationTimeout: 1000,
+  // Enable debug logging for the build
+  logging: {
+    level: 'verbose',
+  },
+  // Optionally, we can try to disable the image optimization if it's causing issues, but note that this is a premium feature in Vercel.
+  // images: {
+  //   unoptimized: true
+  // }
 };
 
 module.exports = nextConfig;
