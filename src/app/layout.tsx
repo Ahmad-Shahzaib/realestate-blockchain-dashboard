fec71842e5bbe8bf530f7b84ac6cf61fe8c7127e
waqas-softsuite <waqas@softsuitetech.com>
2025-10-20 12:04:32 +0500

    update upload funcnality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,17 @@
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import { Sidebar } from "@/components/Layouts/sidebar";
-import { Header } from "@/components/Layouts/header";
 import type { PropsWithChildren } from "react";
-import { Providers } from "./providers";
-import { AuthWrapper } from "@/components/Layouts/auth-wrapper";
 import { metadata } from "./metadata";
 import ClientLayout from "./ClientLayout";
-import GlobalLoader from "./loader/GlobalLoader";
-
 export { metadata };
 
 export default function RootLayout({ children }: PropsWithChildren) {
+  
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-       <GlobalLoader />
         <ClientLayout children={children} />
-
       </body>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,12 +7,14 @@ import store from '../redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from '../redux/store/persistor';
 import React from 'react';
+import NextNProgress from 'nextjs-progressbar';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider defaultTheme="light" attribute="class">
+            <NextNProgress />
           <SidebarProvider>
             {children}
           </SidebarProvider>
